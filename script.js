5fb483c99b9df0e435c3c500fbb0e905914835b3
Jewell Scott <jewelljrscott@gmail.com>
2022-03-30 18:26:23 -0400

    Refactor playRound function

--- script.js
+++ script.js
@@ -14,63 +14,32 @@ function computerPlay() {
 
 const playerSelection = "ROCK";
 const computerSelection = computerPlay();
-console.log(playerSelection);
-console.log(computerSelection);
+
+console.log(`You chose ${playerSelection}`);
+console.log(`Computer chose ${computerSelection}`);
 console.log(playRound(playerSelection, computerSelection));
 
-
 function playRound(playerSelection, computerSelection) {
-    const r = "ROCK";
-    const s = "SCISSORS";
-    const p = "PAPER";
-
-    const ps = playerSelection;
-    const cs = computerSelection;
 
     let playerScore = 0;
     let computerScore = 0;
 
+    if (playerSelection == computerSelection) {
+        console.log("Whoa! It's a draw.");
+    } else if (playerSelection == "ROCK" && computerSelection == "SCISSORS" || playerSelection == "SCISSORS" && computerSelection == "PAPER" || playerSelection == 
+    "PAPER" && computerSelection == "ROCK" ) {
+        console.log(`You win! ${playerSelection} beats ${computerSelection}!`)
+        playerScore++;
+    } else {
+        console.log(`Boo, you lose! ${computerSelection} beats ${[playerSelection]}.`)
+        computerScore++;
+    }
+
     let scores = [[`Computer`, computerScore], [`Player`, playerScore]];
 
-    let result;
-    let roundWinner;
-
-    if (ps == cs) {
-        console.log("Whoa! It's a draw.");
-       playerScore += 0; 
-    } else if (ps == r && cs == p) {
-        console.log("You lose! PAPER beats ROCK.");
-        computerScore++;
-    } else if (ps == r && cs == s) {
-        console.log("You win! ROCK beats SCISSORS!");
-        playerScore++
-    } else if (ps == p && cs == s) {
-        console.log("You lose! PAPER beats SCISSORS.");
-        computerScore++;
-    } else if (ps == p && cs == r) {
-        console.log("You win! PAPER beats ROCK!");
-        computerScore++;
-    } else if (ps == s && cs == r) {
-        console.log("You lose! ROCK beats SCISSORS.");
-        computerScore++;
-    } else if (ps == s && cs == p) {
-        console.log("You win! SCISSORS beats PAPER!");
-        playerScore++;
-    }
-
-    if (playerScore > computerScore) {
-        roundWinner = "Player";
-        console.log(`${roundWinner} wins the round with ${playerSelection}!`);
-    } else if (computerScore > playerScore) {
-        roundWinner = "Computer";
-        console.log(`${roundWinner} wins the round with ${computerSelection}!`);
-    } else {
-        console.log("No one won this round!");
-    }
-    
-    // no scores are populating in this table, so the ++ must not be accruing properly.
+    console.log("Current Score:");
     console.table(scores);
-    return result;
+    return
 }
 
 // function playGame() {
